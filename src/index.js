3f13f10aeba27da9de08b340d740ad77723c0b92
Steve Anderson <prototype909@msn.com>
2020-08-26 15:53:11 -0600

    start build on recipeCard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-document.addEventListener('DOMContentLoaded', ()=> alert('hi'))
+const RECIPES_URL = "http://localhost:3000/recipes"
+
+class Recipe (
+    constructor(title, imageLink, recipeLink, ingredients ) {
+        this.title = title;
+        this.imageLink = imageLink;
+        this.recipeLink = recipeLink;
+        this.ingredients = ingredients;
+    }
+
+    createRecipeCard() {
+        const carContainer = document.getElementById('recipe-card-container')
+        const card = document.createElement('div')
+        card.className = "card"
+        const title = document.createElement('h1')
+        title.innerHTML = this.title
+        card.appendChild(title)
+        const img = document.getElementById('img')
+        img.src = this.imageLink
+        card.appendChild(img)
+        const link = document.createElement('lnk')
+        link.src = this.recipeLink
+        
+    }
+)

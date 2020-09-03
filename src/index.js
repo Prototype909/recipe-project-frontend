7f98efc4fac5af6a6f238c70b56fd70147199cee
Steve Anderson <prototype909@msn.com>
2020-09-03 17:50:44 -0600

    organization

--- src/index.js
+++ src/index.js
@@ -120,7 +120,6 @@ function toggleDropDown() {
 // }
 
 function populateIngredientDropDown(data) {
-  data.sort((a, b) => (a.attributes.name.toUpperCase() > b.attributes.name.toUpperCase()) ? 1 : -1)
   for (ingredient of data) {
     let option = document.createElement("option")
     option.value = ingredient.attributes.name
